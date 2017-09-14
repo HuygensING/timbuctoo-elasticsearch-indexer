ba0755b68909866b65a6a7e0d9bae892d0fba862
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-09-14 10:02:03 +0200

    Implement custom format for person names

--- src/elasticSearchDataFormatter.ts
+++ src/elasticSearchDataFormatter.ts
@@ -1,22 +1,61 @@
 export class ElasticSearchDataFormatter {
   public formatData(data: { [key: string]: any }, config: { [key: string]: any }): {} {
+    JSON.stringify(data);
     for (const key of Object.getOwnPropertyNames(data)) {
-      if (!key.startsWith("@") && data[key] != null) {
-        const type = getProperty(data[key], "type");
-        if (type != null) {
-          switch (type) {
-            default:
-              formatDefault(data, key);
+      const property = data[key]
+      if (!key.startsWith("@") && property != null) {
+        if (Object.getOwnPropertyNames(property).indexOf("type") >= 0) {
+          this.formatField(property);
+        }
+        else if (property instanceof Array) {
+          for (const item of property) {
+            if (Object.getOwnPropertyNames(item).indexOf("type") >= 0) {
+              this.formatField(item);
+            }
+            else {
+              this.formatData(item, config);
+            }
           }
+        }
+        else {
+          this.formatData(property, config);
         }
       }
     }
 
     return data;
   }
+
+  private formatField(field: { [key: string]: any }) {
+    switch (field.type) {
+      case "http://timbuctoo.huygens.knaw.nl/datatypes/person-name":
+        formatPersonName(field)
+        break
+      default:
+        formatDefault(field);
+    }
+  }
 }
 
-function formatDefault(data: { [key: string]: any }, fieldName: string): void {
+function formatDefault(field: { [key: string]: any }): void {
+}
+
+function formatPersonName(field: { [key: string]: any }): void {
+  const value = field.value;
+  console.log("personname: ", value);
+  if (value != null) {
+    const parsedValue = JSON.parse(value);
+    if (Object.getOwnPropertyNames(parsedValue).indexOf("components") >= 0 && parsedValue.components instanceof Array) {
+      field.value = removeTei(parsedValue.components);
+    } else {
+      field.value = "¯\_(ツ)_/¯"; // default value for unparsable person names
+    }
+  }
+}
+
+function removeTei(components: [{ type: string, value: string }]): string[] {
+
+  return components.map(component => component.value);
 }
 
 function getProperty(data: { [key: string]: any }, propName: string): any {
