39d3ba714adbd81cd6e3116a2fcabe3ca6749d57
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-10-05 16:03:36 +0200

    Update test

--- src/queryGeneratorTest.ts
+++ src/queryGeneratorTest.ts
@@ -51,7 +51,7 @@ function testBuildQueryForCollection() {
     };
 
 
-  const expectedQuery = "{ dataSets { DUMMY__clusius { clusius_PersonsList { items { tim_hasDeathPlace { tim_country { value } tim_name { value } } tim_gender { value } tim_birthDate { value } tim_deathDate { value } tim_names { items { value } } } nextCursor } } } }";
+  const expectedQuery = "{ dataSets { DUMMY__clusius { clusius_PersonsList { items { uri tim_hasDeathPlace { tim_country { value } tim_name { value } } tim_gender { value } tim_birthDate { value } tim_deathDate { value } tim_names { items { value } } } nextCursor } } } }";
 
   const actual = buildQueryForCollection("DUMMY__clusius", "clusius_PersonsList", input);
 
