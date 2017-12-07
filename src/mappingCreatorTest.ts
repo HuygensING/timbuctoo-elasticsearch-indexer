5408c47609e8108789d1a29cacbb7b5258a00c33
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-12-07 11:03:10 +0100

    Add test for config without full text

--- src/mappingCreatorTest.ts
+++ src/mappingCreatorTest.ts
@@ -69,4 +69,61 @@ function testCreateMapping() {
   console.log("test createMapping succeeded");
 }
 
+function testCreateMappingWithoutFullText() {
+  const personDesc: {} =  {
+    "collectionListId": "clusius_PersonsList",
+    "indexConfig": {
+      "facet": [
+        {
+          "paths": [
+            "clusius_Persons||tim_namesList.ITEMS||items.VALUE||value"
+          ],
+          "type": "MultiSelect"
+        },
+        {
+          "paths": [
+            "clusius_Persons||tim_hasDeathPlace.clusius_Places||tim_country.VALUE||value",
+            "clusius_Persons||tim_hasDeathPlace.clusius_Places||tim_name.VALUE||value"
+          ],
+          "type": "Hierarchical"
+        },
+        {
+          "paths": [
+            "clusius_Persons||tim_birthDate.VALUE||value"
+          ],
+          "type": "DateRange"
+        }
+      ],
+      "fullText": []
+    }
+  };
+
+  const expected: {} = {
+    "properties": {
+      "tim_namesList.items.value.raw": {
+        "type": "keyword"
+      },
+      "tim_hasDeathPlace.tim_country.value.raw": {
+        "type": "keyword"
+      },
+      "tim_hasDeathPlace.tim_name.value.raw": {
+        "type": "keyword"
+      },
+      "tim_birthDate.value.raw": {
+        "type": "date"
+      }
+    }
+  };
+
+
+  const actual = mappingCreator.createMapping(personDesc);
+
+  console.log("test createMapping without full text");
+  console.assert(JSON.stringify(expected) === JSON.stringify(actual),
+    "expected:\n" + JSON.stringify(expected) + "\nbut was:\n" + JSON.stringify(actual)
+  );
+  console.log("test createMapping without full text succeeded");
+}
+
 testCreateMapping();
+testCreateMappingWithoutFullText();
